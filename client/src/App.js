a7964e1f9d7228c3bfe26cda194e091c82437c94
Saifurrahmanemon <saifemon789@gmail.com>
2020-11-23 07:08:49 +0000

    added reg

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
+
+import { Container } from "semantic-ui-react";
+function App() {
+    return (
+        <Router>
+            <Container>
+                <MenuBar />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+            </Container>
+        </Router>
+    );
+}
+
+export default App;
